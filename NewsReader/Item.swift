1ecb98e907f1ca235a0d126982cb8ee691e56a25
mht-rentaro-matsushita <r.matsushita@mht-code.com>
2018-07-24 18:54:41 +0900

    初回コミット

---
+++ NewsReader/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  NewsReader
+//
+//  Created by 松下廉太郎 on 2018/07/23.
+//  Copyright © 2018年 manhattan.code. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title = ""
+    var link = ""
+}
